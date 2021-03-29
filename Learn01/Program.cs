d58f770ab3ae72c2336405daf429b1ceb3d4225e
Федотов Андрей Сергеевич <azodtm@gmail.com>
2021-03-29 16:07:19 +0500

    1 задание готово. программа считает сумму 2 чисел

--- Learn01/Program.cs
+++ Learn01/Program.cs
@@ -4,9 +4,49 @@ namespace Learn01
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            decimal[] numbers = new decimal[2];
+            for (int i = 0; i < numbers.Length; i++)
+            { 
+                while (true)
+                {
+                    
+                    Console.WriteLine("Enter {0} from {1} for sum",i+1,numbers.Length);
+                    try
+                    {
+                        numbers[i] = Convert.ToDecimal(Console.ReadLine());
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(ex);
+                        Console.ResetColor();
+                    }
+                }
+            }
+
+            decimal finalSum = 0;
+            foreach (decimal number in numbers)
+            {
+                try
+                {
+                    finalSum += number;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            Console.WriteLine("Sum = {0}",finalSum);
+            
+                       
         }
+
+       
     }
 }
