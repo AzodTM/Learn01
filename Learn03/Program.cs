64f1d4046b4a1fa7eb1291c122441483aa16db65
Федотов Андрей Сергеевич <azodtm@gmail.com>
2021-03-30 19:34:10 +0500

    начато 3 задание. переброс на домашний пк

--- Learn03/Program.cs
+++ Learn03/Program.cs
@@ -4,9 +4,41 @@ namespace Learn03
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string expression = "";
+            decimal test = 0;
+            
+
+            while(true)
+            {                
+                Console.WriteLine("THE CALC!");
+                
+
+                bool inputIsCorect = false;
+                while (!inputIsCorect)
+                {
+                    expression = Console.ReadLine();
+                    foreach (char symvol in expression)
+                    { 
+                        if(((int)symvol > 47 && (int)symvol < 58) || (int)symvol == 44)
+                        {
+                            inputIsCorect = true;
+                        }
+                        else
+                        {
+                            inputIsCorect = false;
+                            break;
+                        }
+                        
+                    }
+                    
+                }
+                Console.WriteLine(test);
+               
+
+            }
         }
     }
 }
